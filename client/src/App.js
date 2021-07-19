9114dc2ff57d911298383dc911645ac1ab9695b2
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-19 18:00:06 -0400

    Update1

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	useParams,
+	useRouteMatch,
+} from "react-router-dom";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+export default function App() {
+	const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+	const [movieList, setMovieList] = useState([]);
+	// const { path, url } = useRouteMatch();
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	useEffect(() => {
+		const getMovies = () => {
+			axios
+				.get("http://localhost:5000/api/movies")
+				// Study this endpoint with Postman
+				// Study this response with a breakpoint or log statements
+				// and set the response data as the 'movieList' slice of state
+				.then((response) => {
+					setMovieList(response.data);
+				})
+				.catch((error) => {
+					console.error("Server Error", error);
+				});
+		};
+		getMovies();
+	}, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+	const addToSavedList = (id) => {
+		// This is stretch. Prevent the same movie from being "saved" more than once
+	};
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<SavedList
+					list={
+						[
+							/* This is stretch */
+						]
+					}
+				/>
+				<Route exact path="/" component={"MovieList"}>
+					<MovieList movies={movieList} />
+				</Route>
+				<Route path="/movies/:id">
+					<Movie />
+				</Route>
+			</div>
+		</Router>
+	);
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Movie from "./Movie";
 
 export default function MovieList(props) {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="movie-list">
+			{props.movies.map((movie) => (
+				<MovieDetails key={movie.id} movie={movie} />
+			))}
+		</div>
+	);
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
+	const { title, director, metascore, key } = props.movie;
+	const params = useParams();
+	return (
+		<div
+			className="movie-card"
+			onClick={(e) => {
+				console.log(props.movie);
+				return <Link to={`/movies/${key}`} />;
+			}}
+		>
+			<h2>{title}</h2>
+			<Link to={`/movies/$${key}`} />
+			<div className="movie-director">
+				Director: <em>{director}</em>
+			</div>
+			<div className="movie-metascore">
+				Metascore: <strong>{metascore}</strong>
+			</div>
+		</div>
+	);
 }
